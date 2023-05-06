8131ab5f57a4bf3b70dc14ad949c65ac2601a44d
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-06 09:38:57 +0000

    Sat, May 6, 2023, 3:08 PM +05:30

---
+++ Module5/Question1.py
@@ -0,0 +1,9 @@
+"""
+On what input of a and b this program will display “I win the game”? Find the value of a and b.
+
+if (a & b >= 1) | (a^b <=1) :
+    print("I win the game")
+else:
+    print("I lose the game")
+"""
+
