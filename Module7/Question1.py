4b5477683e5f9e72a28649ac0cccf77a2bae642c
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-06 10:16:22 +0000

    Sat, May 6, 2023, 3:46 PM +05:30

---
+++ Module7/Question1.py
@@ -0,0 +1,7 @@
+"""
+Asha has a huge collection of country currencies. She decided to count the total number of
+distinct country currencies in her collection. She asked for your help. You pick the currency one
+by one from a stack of country currencies.
+Find the total number of distinct country currencies.
+
+"""
