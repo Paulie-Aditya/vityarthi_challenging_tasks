b4f0a933d6fccf21420ac6b94a177efc00b5d96c
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-06 09:51:21 +0000

    Sat, May 6, 2023, 3:21 PM +05:30

--- Module5/Question2.py
+++ Module5/Question2.py
@@ -22,7 +22,16 @@ a = 100, b = 100
 Logic of the Program is as follows:
 
 First, the first parenthesis is checked.
-We check the condition of (a & b >=1)
+The condition of (a & b >=1) is checked.
+If this is False, the second condition is checked too, since there is an OR operator between the conditions.
+The condition of (a^b <=1) is now checked.
+
+If both of these are False, the else statement is executed.
+That is, "I lose the game" is printed
+
+If either of these are True, the block of code inside the if statement is executed.
+That is, "I win the game" is printed
+
 
 """
 
