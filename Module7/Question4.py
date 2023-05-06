66e9158ea083bbfb4a2590c1b50e6ef0e235d0c3
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-06 10:44:15 +0000

    Sat, May 6, 2023, 4:14 PM +05:30

--- Module7/Question4.py
+++ Module7/Question4.py
@@ -3,8 +3,17 @@ A quiz was conducted in a programming tutorial class for 10 marks. Calculate Mea
 Standard Deviation of the marks scored by minimum seven students in the class. Use the
 appropriate functions provided by Python.
 """
+import math
 
 scores = list(map(int,input("Enter 7 integers with spaces: "),split()))
 
 mean = round((sum(scores) / len(scores)),3)
 
+summation = 0
+for score in scores:
+    summation+=(abs(score-mean))**2
+
+standard_deviation = math.sqrt(summation/len(scores))
+
+print("Mean is:", mean)
+print("Standard Deviation is:", standard_deviation)
