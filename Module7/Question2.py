29ff6ab31b881b8911e8aeafca5b65f1704fc1d2
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-06 10:28:32 +0000

    Sat, May 6, 2023, 3:58 PM +05:30

---
+++ Module7/Question2.py
@@ -0,0 +1,7 @@
+"""
+A startup company named 'MY_Chat' having four employees names as Sunny, Ravi, Vijay and
+Messi having the respected salaries are 25000,23000, 26000, and 30000.
+"""
+
+MY_Chat = {"Sunny": 25000,"Ravi": 23000,"Vijay": 26000,"Messi":30000}
+
