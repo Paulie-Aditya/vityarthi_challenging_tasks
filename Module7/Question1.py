b0dbff87a5dbe997ffc13ae5fd7c95599b82638f
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-06 10:26:25 +0000

    Sat, May 6, 2023, 3:56 PM +05:30

--- Module7/Question1.py
+++ Module7/Question1.py
@@ -7,3 +7,16 @@ Find the total number of distinct country currencies.
 """
 
 n = int(input("Enter Number of Currencies: "))
+countries= []
+distinct = 0
+for i in range(n):
+    country = input("Enter name of country: ")
+    if country in countries:
+        print(distinct)
+    else:
+        distinct +=1
+        print(distinct)
+    countries.append(country)
+
+
+
