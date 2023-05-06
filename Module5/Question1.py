f4c3459d0cedd65da322e6e620088059fb091dff
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-06 09:45:05 +0000

    Sat, May 6, 2023, 3:15 PM +05:30

--- Module5/Question1.py
+++ Module5/Question1.py
@@ -7,3 +7,23 @@ else:
     print("I lose the game")
 """
 
+
+"""
+There are various inputs for which we can 'Win the game'.
+Few of such inputs are:
+
+a = 2, b= 2
+a = 2, b=3
+a = 2, b=10
+a = 100, b = 100
+"""
+
+a = 100
+b = 100
+if (a & b >= 1) | (a^b <=1) :
+    print("I win the game")
+else:
+    print("I lose the game")
+
+
+
