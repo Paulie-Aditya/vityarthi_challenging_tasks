64c9bf250bef13d043ffb23e280d1bf2d05618c0
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-06 09:28:13 +0000

    Sat, May 6, 2023, 2:58 PM +05:30

---
+++ Module4.py/Question3.py
@@ -0,0 +1,12 @@
+"""
+A car has travelled D kilometers in T hours of time. What is the speed of the car in km/hr?
+Write a python program to demonstrate.
+"""
+
+print("Enter integers for distance and time: ", end = "")
+
+distance,time = list(map(int,input().split()))
+
+speed = round((distance/time),3)
+
+print("The speed of the car in km/h is: ", speed)
