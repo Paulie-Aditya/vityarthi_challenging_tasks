41ff10a6620093772924d04f13e822e217c140db
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-06 09:47:19 +0000

    Sat, May 6, 2023, 3:17 PM +05:30

---
+++ Module5/Question2.py
@@ -0,0 +1,34 @@
+"""
+On what input of a and b this program will display “I win the game”? Find the value of a and b.
+
+if (a & b >= 1) | (a^b <=1) :
+    print("I win the game")
+else:
+    print("I lose the game")
+"""
+
+
+"""
+There are various inputs for which we can 'Win the game'.
+Few of such inputs are:
+
+a = 2, b= 2
+a = 2, b=3
+a = 2, b=10
+a = 100, b = 100
+"""
+
+"""
+Logic of the Program is as follows:
+
+First, the first parenthesis is checked.
+We check the condition of (a & b >=1)
+
+"""
+
+a = 100
+b = 100
+if (a & b >= 1) | (a^b <=1) :
+    print("I win the game")
+else:
+    print("I lose the game")
