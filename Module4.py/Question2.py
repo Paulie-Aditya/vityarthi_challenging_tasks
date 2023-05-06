2b0432b5a3ef6bbe86321b1eb4d609b445388e6f
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-06 09:25:06 +0000

    Sat, May 6, 2023, 2:55 PM +05:30

--- Module4.py/Question2.py
+++ Module4.py/Question2.py
@@ -3,3 +3,14 @@ Mr. Ashok Goel has spent ‘n’ units of effort in preparing for UPSC examinati
 Can you calculate using a Python program how many hours, minutes and seconds he has
 spent on attaining success.
 """
+
+n = int(input())
+
+hours = n//3600
+minutes = (n%3600 )//60
+seconds = ((n%3600)%60)
+
+print("Hours: ",hours, end = "\n")
+print("Minutes: ",minutes, end = "\n")
+print("Seconds: ",seconds, end = "\n")
+
