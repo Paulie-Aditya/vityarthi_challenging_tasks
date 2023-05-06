072c01576c311da5d78c156136d760c3d155ee3f
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-06 09:22:04 +0000

    Sat, May 6, 2023, 2:52 PM +05:30

---
+++ Module4.py/Question2.py
@@ -0,0 +1,5 @@
+"""
+Mr. Ashok Goel has spent ‘n’ units of effort in preparing for UPSC examination in a year.
+Can you calculate using a Python program how many hours, minutes and seconds he has
+spent on attaining success.
+"""
