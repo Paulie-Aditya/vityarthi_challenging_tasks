69bfe5d995b0cfb283c50dc3c15cd40a1d3458f9
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-06 10:04:11 +0000

    Sat, May 6, 2023, 3:34 PM +05:30

---
+++ Module6/Question1.py
@@ -0,0 +1,33 @@
+"""
+What is the output of the following Python code block? Explain the logic of the given code.
+Explain the logic of the given program and write the output as well.
+
+a=2
+b=12
+c=1
+d=a<b>c-1
+d=2<12>1-1
+d=2<12>0
+d=1>0
+print(d)
+"""
+
+
+
+"""
+Output: True
+
+"""
+
+"""
+Logic of the program:
+
+First we have declared a = 2, b = 12, c = 1
+
+We set d to a<b>c-1
+This means:
+    First c-1 is calculated since - has higher operator precedence.
+    Now the condition is checked from left-to-right since all operators have same precedence
+    
+
+"""
